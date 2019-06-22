32cd72d20e00adde064baf1059514d6131b29762
taramouse <nickykeyse@gmail.com>
2019-06-22 15:48:16 +0100

    Added projects.

--- src/store.js
+++ src/store.js
@@ -5,8 +5,34 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    title: 'Game Stats',
-    msg: 'This is vuex state.'
+    title: 'Projects',
+    msg: 'This is the current vuex state.',
+    projects: [
+      {
+        id: '1',
+        title: 'First Project',
+        desc: 'The first project.',
+        color: 'red'
+      },
+      {
+        id: '2',
+        title: 'Second Project',
+        desc: 'The second project.',
+        color: 'green'
+      },
+      {
+        id: '3',
+        title: 'Third Project',
+        desc: 'The third project, it has a longer description than the other projects',
+        color: 'blue'
+      },
+      {
+        id: '4',
+        title: 'Fourth Project',
+        desc: 'The fourth project.',
+        color: 'yellow'
+      }
+    ]
   },
   getters: {
     title (state) {
@@ -14,6 +40,9 @@ export default new Vuex.Store({
     },
     msg (state) {
       return state.msg
+    },
+    projects (state) {
+      return state.projects
     }
   },
   mutations: {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,24 @@
 <template>
-  <v-layout>
-    <h1>{{title}}</h1>
-    <h3>{{msg}}</h3>
-  </v-layout>
+  <v-container>
+    <v-layout>
+      <h1>{{title}}</h1>
+    </v-layout>
+    <v-layout>
+      <h3>{{msg}}</h3>
+    </v-layout>
+    <v-card flat class="pa-3" v-for="project in projects" :key="project.id">
+      <v-layout row wrap>
+        <v-flex xs4 md4 class="info">
+          <div class="title">{{project.title}}</div>
+        </v-flex>
+        <v-flex xs8 md6>
+          <div>{{project.desc}}</div>
+        </v-flex>
+      </v-layout>
+      <v-divider></v-divider>
+    </v-card>
+  </v-container>
+
 </template>
 
 <script>
@@ -14,6 +30,9 @@ export default {
     },
     msg () {
       return this.$store.getters.msg
+    },
+    projects () {
+      return this.$store.getters.projects
     }
   }
 }
