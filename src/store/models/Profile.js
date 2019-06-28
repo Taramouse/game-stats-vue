b30c2661da2bfcb5e2962cc1c22d2faf444bf493
taramouse <nickykeyse@gmail.com>
2019-06-28 18:04:44 +0100

    Added ORM models.

---
+++ src/store/models/Profile.js
@@ -0,0 +1,20 @@
+// Profle Model
+import { Model } from '@vuex-orm/core'
+import User from './User'
+
+export default class Profile extends Model {
+  static entity = 'profiles'
+
+  static fields () {
+    // `this.belongsTo` is for belongs to relationship. The first argument
+    // is the Model class, and second is the foreign key.
+    return {
+      id: this.attr(null),
+      user_id: this.attr(null),
+      title: this.attr(''),
+      body: this.attr(''),
+      published: this.attr(false),
+      author: this.belongsTo(User, 'user_id')
+    }
+  }
+}
